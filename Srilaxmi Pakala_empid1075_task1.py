6ea57b660757a912539e6ba528d17ac5c88797f9
srilaxmiammu <srilaxmipakala92@gmail.com>
2026-05-28 11:23:52 +0530

    Add files via upload

---
+++ Srilaxmi Pakala_empid1075_task1.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+fav_subject = input("Enter your favourite subject: ")
+
+birth_year = 2024 - age
+
+print("\n===== PERSONAL PROFILE CARD =====")
+print(f"Name           : {name}")
+print(f"Age            : {age}")
+print(f"City           : {city}")
+print(f"Favourite Subject : {fav_subject}")
+print(f"Birth Year     : {birth_year}")
+print("=================================")
